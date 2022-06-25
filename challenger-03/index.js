6700f0a44db3212a54194adfdddefab2b15f2a8c
Igor Vieira <igor.p.r.vieira@gmail.com>
2022-06-25 18:48:57 -0300

    Update index.js

--- challenger-03/index.js
+++ challenger-03/index.js
@@ -1,4 +1,4 @@
-function convertTimeinMilliseconds(hour, minutes, seconds) {
+function convertTimeInMilliseconds(hour, minutes, seconds) {
   const ms = seconds * 1000
   const min = minutes * 60 * 1000
   const h = hour * 60 * 60 * 1000
@@ -6,4 +6,4 @@ function convertTimeinMilliseconds(hour, minutes, seconds) {
   return ms + min + h
 }
 
-module.exports = convertTimeinMilliseconds
+module.exports = convertTimeInMilliseconds
