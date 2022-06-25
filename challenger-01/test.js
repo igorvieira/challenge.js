1df0ed49f617fa03f6aacefab9343bd348892297
Igor Vieira <igor.p.r.vieira@gmail.com>
2022-06-25 17:41:54 -0300

    Update test.js

--- challenger-01/test.js
+++ challenger-01/test.js
@@ -1,8 +1,13 @@
-const bool2Word = require("./")
+const bool2Word = require('./')
 
-describe("bool2Word", () => {
-  it("should return true our false after call the function", () => {
+describe('bool2Word', () => {
+  it('should return the correct value about this function', () => {
     expect(bool2Word(true)).toBe('Yes')
     expect(bool2Word(false)).toBe('No')
-   });
-});
+  })
+
+  it('should not return the correct value about this function', () => {
+    expect(bool2Word(false)).not.toBe('Yes')
+    expect(bool2Word(true)).not.toBe('No')
+  })
+})
