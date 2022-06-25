91c940ed7637b56c92f97c0d16fec70ba265b762
Igor Vieira <igor.p.r.vieira@gmail.com>
2022-06-25 18:38:22 -0300

    Update test.js

--- challenger-02/test.js
+++ challenger-02/test.js
@@ -1,18 +1,24 @@
-const formSentence = require("./index")
+const formSentence = require('./')
 
-describe("formSentence", () =>  {
+describe('formSentence', () => {
 
-  it ("should return empty string for empty array.", () => {
-   expect(formSentence([])).toBe("");
-  });
+  it("should return empty string for a empty array", () => {
+    expect(formSentence([])).toBe("")
+  })
 
-  it ("should return a unique word.", () => {
-   expect(formSentence(["hello"])).toBe("hello");
-  });
+  it("should return a single world", () => {
+    expect(formSentence(['hello'])).toBe("hello")
+  })
 
-  it ("should return multiple sentences", () =>  {
-   expect(formSentence(["hello", "world"])).toBe("hello world");
-   expect(formSentence(["hello", "amazing", "world"])).toBe("hello amazing world");
-   expect(formSentence(["this", "is", "a", "really", "long", "sentence"])).toBe("this is a really long sentence");
-  });
-});
+  it("should return multiple sentences", () => {
+    expect(formSentence(["hello", "world"])).toBe("hello world")
+    expect(formSentence(["hello", "amazing", "world"])).toBe("hello amazing world")
+    expect(formSentence(["this", "is", "a", "really", "long", "sentence", "for", "sure"])).toBe("this is a really long sentence for sure")
+  })
+
+  it("should not return a incorrect value", () => {
+    expect(formSentence(["hello", "world"])).not.toBe([])
+    expect(formSentence(["hello", "amazing", "world"])).not.toBe("")
+    expect(formSentence(["this", "is", "a", "really", "long", "sentence", "for", "sure"])).not.toBe("hello world")
+  })
+})
