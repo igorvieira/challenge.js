4f8b3dd304bbc1f756981b0d7f0fcf63ca2257d8
Igor Vieira <igor.p.r.vieira@gmail.com>
2022-06-25 22:37:58 -0300

    Update test.js

--- challenger-03/test.js
+++ challenger-03/test.js
@@ -1,11 +1,18 @@
-const convertTimeInMilliseconds = require("./index")
+const convertTimeInMilliseconds = require('./')
 
-describe("convertTimeinMilliseconds", () => {
-  it("should converte hour, minutes, and seconds to milliseconds", () => {
+describe('convertTimeInMilliseconds', () => {
+  it('should converte hour, minutes, and seconds to milliseconds', () => {
     expect(convertTimeInMilliseconds(0,1,1)).toBe(61000)
     expect(convertTimeInMilliseconds(1,1,1)).toBe(3661000)
     expect(convertTimeInMilliseconds(0,0,0)).toBe(0)
     expect(convertTimeInMilliseconds(1,0,1)).toBe(3601000)
     expect(convertTimeInMilliseconds(1,0,0)).toBe(3600000)
-  });
-});
+  })
+
+  it('should not converte hour, minutes, and seconds to milliseconds into another typeof', () => {
+    expect(convertTimeInMilliseconds(0,1,1)).not.toBe('')
+    expect(convertTimeInMilliseconds(0,1,1)).not.toBe([])
+    expect(convertTimeInMilliseconds(0,1,1)).not.toBe(0)
+    expect(convertTimeInMilliseconds(0,1,1)).not.toBe({})
+  })
+})
